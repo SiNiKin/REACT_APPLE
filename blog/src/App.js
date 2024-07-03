c3896ed7603bb48ea2c7f4a1e83d23e81f2ebf20
SiNiKin <tlsdl99@gmail.com>
2024-07-04 00:13:05 +0900

    map

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Com from './component/Com.js';
+import Prac from './component/Prac.js';
 
 function App() {
   return (
     <div className="App">
-      <Com />
+      {/* <Com /> */}
 
       {/* {
         [1,2,3].map(function(){
           return <Com />
         })
       } */}
+
+      <Prac />
     </div>
   );
 }
